96be3ba2ad4fb470ccc7e16a9e99238e5420442a
dawnight <weizhiqimail@foxmail.com>
2019-05-27 21:54:09 +0800

    删除不需要的模块

--- webpack/webpack-react/src/video.js
+++
@@ -1,4 +0,0 @@
-console.log('这是一个非常大的文件资源');
-export function getName() {
-  return 'get video.js name';
-}
--- webpack/webpack-react/webpack.config.prod.js
+++
@@ -1,7 +0,0 @@
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.config.base');
-
-module.exports = merge(baseConfig, {
-  mode: 'development'
-});
-
